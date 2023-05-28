e5a977118eb574bba0e81f22ae356504c5f648cc
alex <alexhyisen@gmail.com>
2023-05-28 21:49:41 +0800

    build baseline

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
+import pino, {TransportTargetOptions} from "pino";
+
 console.log("Hello world.");
+
+function createLogger() {
+    const targets: TransportTargetOptions[] = [
+        {target: 'pino/file'} as TransportTargetOptions,
+        {target: 'pino/file', options: {}, level: 'info'}
+    ];
+    const levels = {
+        apple: 80,
+        banana: 75,
+        cherry: 70,
+        dates: 65,
+        eggplant: 60,
+    };
+    return pino({
+        transport: {targets: targets},
+        customLevels: levels
+    })
+}
+
+const logger = createLogger();
+logger.info("echo");
